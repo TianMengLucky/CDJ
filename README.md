ab3525bed47c88386c63269d22d90c6d63e8fcbb
Bilibili_MengChu <2133404320@qq.com>
2024-06-16 14:39:59 +0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 一个基于[TownOfNext](https://github.com/KARPED1EM/TownOfNext)车队姬原理实现的服务端
+可通过TestCDJ模拟客户端发送进行测试
---
+++ TestCDJ/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Test CDJ");
+Create:
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+Console.WriteLine("IP:");
+var ip = Console.ReadLine();
+Console.WriteLine("Port:");
+var port = int.Parse(Console.ReadLine()!);
+await socket.ConnectAsync(ip!, port);
+
+
+send:
+Console.WriteLine("Code:");
+var Code = Console.ReadLine();
+Console.WriteLine("Version:");
+var Version = Console.ReadLine();
+Console.WriteLine("PlayerCount:");
+var PlayerCount = Console.ReadLine();
+Console.WriteLine("Language:");
+var Language = Console.ReadLine();
+Console.WriteLine("ServerName:");
+var ServerName = Console.ReadLine();
+Console.WriteLine("HostName:");
+var HostName = Console.ReadLine();
+Console.WriteLine("BuildName:");
+var BuildName = Console.ReadLine();
+
+var stings = $"{Code}|{Version}|{PlayerCount}|{Language}|{ServerName}|{HostName}";
+if (BuildName != null)
+{
+    stings += $"|{BuildName}";
+}
+
+await socket.SendAsync(Encoding.Default.GetBytes(stings));
+socket.Dispose();
+
+Console.WriteLine("Parse Option\n 1.Send 2.Create");
+var option = int.Parse(Console.ReadLine()!);
+if (option == 1)
+{
+    goto send;
+}
+
+if (option == 2)
+{
+    goto Create;
+}
+
+Console.ReadKey();
